ddb79cee2f7bd6dd2e171260b6eb243437996c53
226095568-Iyambo <ndinaifpio@gmail.com>
2026-08-03 10:11:34 +0200

    fixing my error

--- week1/main.c
+++ week1/main.c
@@ -4,16 +4,15 @@ int main(){
     char mayor[80];
     int population;
 
-    printf("Welcome to Windhoek Municipality: \n");
+    printf("Welcome to Windhoek Municipality\n\n");
 
-    printf("Enter the name of the municipality: \n");
-    scanf("%49s", &municipality);
+    printf("Enter the name of the municipality: "); 
+    scanf(" %49[^\n]", municipality);
 
-    printf("Enter the name of the mayor: \n");
-    scanf("%49s", &mayor); 
+    printf("Enter the name of the mayor: ");
+    scanf(" %49[^\n]", mayor); 
 
-
-    printf("Enter the population: \n");
+    printf("Enter the population: ");
     scanf("%d", &population);
 
     printf("municipality: %s\n", municipality);
